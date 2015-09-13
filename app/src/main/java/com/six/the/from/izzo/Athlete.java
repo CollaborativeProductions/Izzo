d65cf37c8ea9daaad7edda7a78faab1b31e15a0b
sshariff01 <shoabe.shariff@gmail.com>
2015-09-13 15:11:27 -0700

    New Athlete class [#103275484]

---
+++ app/src/main/java/com/six/the/from/izzo/Athlete.java
@@ -0,0 +1,70 @@
+package com.six.the.from.izzo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Athlete {
+    private int id;
+    private String uuid;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
+    private Set<Integer> groups;
+
+    public Athlete(String uuid, String first_name, String last_name, String phone_number) {
+        setUuid(uuid);
+        setFirstName(first_name);
+        setLastName(last_name);
+        setPhoneNumber(phone_number);
+        this.groups = new HashSet();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String UUID) {
+        this.uuid = UUID;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public Set<Integer> getGroups() {
+        return groups;
+    }
+
+    public void addGroup(int groupId) {
+        this.groups.add(groupId);
+    }
+}
