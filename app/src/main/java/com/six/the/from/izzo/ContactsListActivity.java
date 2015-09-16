bdb630290cf80f09045375542aea92b4f0e283e4
sshariff01 <shoabe.shariff@gmail.com>
2015-09-15 22:25:23 -0700

    Dynamically update search query to find contacts. [#103285404]

--- app/src/main/java/com/six/the/from/izzo/ContactsListActivity.java
+++ app/src/main/java/com/six/the/from/izzo/ContactsListActivity.java
@@ -2,36 +2,71 @@ package com.six.the.from.izzo;
 
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 
 public class ContactsListActivity extends ActionBarActivity {
+    private String searchString;
+    private Cursor cur;
+    private SimpleCursorAdapter scAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
+        init();
+        setEditTextChangedListener();
+    }
 
-        Cursor cur = getContacts();
+    public void init() {
+        ListView lv = (ListView) findViewById(R.id.contacts_list_view);
+        cur = getContacts();
         String[] fields = new String[] {
                 ContactsContract.Data.DISPLAY_NAME
         };
-        SimpleCursorAdapter scAdapter =
+        scAdapter =
                 new SimpleCursorAdapter(this,
                         R.layout.contact_list_item,
                         cur,
                         fields,
                         new int[]{R.id.name_entry},
                         0);
-
-        ListView lv = (ListView) findViewById(R.id.contacts_list_view);
         lv.setAdapter(scAdapter);
+
+        searchString = new String();
+    }
+
+    public void setEditTextChangedListener() {
+        EditText etSearchString = (EditText) findViewById(R.id.et_search_string);
+        etSearchString.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable text) {
+            }
+
+            public void beforeTextChanged(CharSequence text, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence text, int start,
+                                      int before, int count) {
+                setSearchString(text.toString());
+                updateCursor(getContacts());
+            }
+        });
+    }
+
+    private void updateCursor(Cursor swapCursor) {
+        scAdapter.swapCursor(swapCursor);
     }
 
     private Cursor getContacts() {
@@ -40,11 +75,22 @@ public class ContactsListActivity extends ActionBarActivity {
         String[] projection =
                 new String[]{ ContactsContract.Contacts._ID,
                         ContactsContract.Contacts.DISPLAY_NAME };
-        String selection = null;
-        String[] selectionArgs = null;
+        String selection =
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?" :
+                        ContactsContract.Contacts.DISPLAY_NAME + " LIKE ?";
+        String[] selectionArgs = { "%" + getSearchString() + "%" };
         String sortOrder = ContactsContract.Contacts.DISPLAY_NAME +
                 " COLLATE LOCALIZED ASC";
         return getContentResolver().query(uri, projection, selection, selectionArgs, sortOrder);
+    }
+
+    private void setSearchString(String text) {
+        searchString = text;
+    }
+
+    private String getSearchString() {
+        return searchString;
     }
 
     @Override
