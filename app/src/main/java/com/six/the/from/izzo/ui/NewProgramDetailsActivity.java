b19a67afda308664aa1603401c46fb6eea3f1ef5
sshariff01 <shoabe.shariff@gmail.com>
2015-11-08 13:57:01 -0800

    Build out non-functional UI of NewProgramDetailsActivity [#107670670]

--- app/src/main/java/com/six/the/from/izzo/ui/NewProgramDetailsActivity.java
+++ app/src/main/java/com/six/the/from/izzo/ui/NewProgramDetailsActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.six.the.from.izzo.R;
 
@@ -13,6 +17,39 @@ public class NewProgramDetailsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_program_details);
+        initViews();
+    }
+
+    private void initViews() {
+        initCardioListView();
+        initWeightTrainingListView();
+    }
+
+    private void initCardioListView() {
+        ListView listView = (ListView) findViewById(R.id.lv_cardio_exercises);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1);
+        arrayAdapter.add("Running");
+        arrayAdapter.add("Swimming");
+        listView.setAdapter(arrayAdapter);
+    }
+
+    private void initWeightTrainingListView() {
+        ListView listView = (ListView) findViewById(R.id.lv_weighttraining_exercises);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1);
+        arrayAdapter.add("Bench Press");
+        arrayAdapter.add("1-Arm Tricep Curl");
+        arrayAdapter.add("Leg Extension");
+        arrayAdapter.add("Squat (Olympic)");
+        arrayAdapter.add("Hammer Curl");
+        arrayAdapter.add("Leg Press");
+        listView.setAdapter(arrayAdapter);
+    }
+
+    public void addNewWeightTrainingExercise(View v) {
+        Toast.makeText(getApplicationContext(), "this is my Toast message!!! =)",
+                Toast.LENGTH_LONG).show();
     }
 
     @Override
